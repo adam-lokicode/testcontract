ed28852d3bbb69b270230d31fbc4d0911a33db73
adam-lokicode <132405074+adam-lokicode@users.noreply.github.com>
2024-08-06 00:57:55 -0700

    Update ttestcontract.sol

--- ttestcontract.sol
+++ ttestcontract.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.16 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleStorage {
+contract SimpleStorage is ERC20, Ownable(msg.sender) {
     uint storedData;
 
     function set(uint x) public {
